07a03e1fccc317cc9b78c6f3c12c08b40c200af5
Eduardo Marcos <edujugon@hotmail.com>
2017-05-18 15:53:46 +0200

    [Release] V1.0.5

--- tests/OdooTest.php
+++ tests/OdooTest.php
@@ -24,7 +24,7 @@ class OdooTest extends TestCase
 
         $this->odoo = new Odoo();
 
-        //$this->setDemoCredentials();
+        $this->setDemoCredentials();
         $this->createOdooInstance();
     }
 
