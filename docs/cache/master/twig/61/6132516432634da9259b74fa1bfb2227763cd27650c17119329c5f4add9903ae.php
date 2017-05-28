1e20b7c3d5b4accebbd4de1712ac9320e9b29571
Eduardo Marcos <edujugon@hotmail.com>
2017-05-28 20:35:22 +0200

    Commit

---
+++ docs/cache/master/twig/61/6132516432634da9259b74fa1bfb2227763cd27650c17119329c5f4add9903ae.php
@@ -0,0 +1,69 @@
+<?php
+
+/* index.twig */
+class __TwigTemplate_11c4b8436a294f0065b37bb90d405d03327b2d2b9ec96ef88d1bb583bd4fb3dd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = array(
+            'body_class' => array($this, 'block_body_class'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 7
+        return $this->loadTemplate((isset($context["extension"]) || array_key_exists("extension", $context) ? $context["extension"] : (function () { throw new Twig_Error_Runtime('Variable "extension" does not exist.', 7, $this->getSourceContext()); })()), "index.twig", 7);
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if ((isset($context["has_namespaces"]) || array_key_exists("has_namespaces", $context) ? $context["has_namespaces"] : (function () { throw new Twig_Error_Runtime('Variable "has_namespaces" does not exist.', 1, $this->getSourceContext()); })())) {
+            // line 2
+            $context["extension"] = "namespaces.twig";
+        } else {
+            // line 4
+            $context["extension"] = "classes.twig";
+        }
+        // line 7
+        $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 9
+    public function block_body_class($context, array $blocks = array())
+    {
+        echo "index";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  36 => 9,  32 => 7,  29 => 4,  26 => 2,  24 => 1,  18 => 7,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% if has_namespaces %}
+    {% set extension = 'namespaces.twig' %}
+{% else %}
+    {% set extension = 'classes.twig' %}
+{% endif %}
+
+{% extends extension %}
+
+{% block body_class 'index' %}
+", "index.twig", "/Users/Edujugon/Code/Documentation/Documentator/vendor/sami/sami/Sami/Resources/themes/default/index.twig");
+    }
+}
